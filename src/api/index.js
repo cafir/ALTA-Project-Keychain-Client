9f41e806e3da0e4f2827ff9b98f630408d3715c0
Devanada <ydevanada@gmail.com>
2021-11-17 16:50:35 +0700

    Updated baseurl to Heroku

--- src/api/index.js
+++ src/api/index.js
@@ -1,23 +1,28 @@
-import axios from 'axios';
+import axios from "axios";
 
 // const url = 'http://localhost:5000/holders';
 
-const API = axios.create({ baseURL: 'http://localhost:5000' })
+const API = axios.create({
+  baseURL: "https://keychain-backend.herokuapp.com/",
+});
 
 API.interceptors.request.use((req) => {
-    if (localStorage.getItem('profile')) {
-        req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`
-    }
+  if (localStorage.getItem("profile")) {
+    req.headers.Authorization = `Bearer ${
+      JSON.parse(localStorage.getItem("profile")).token
+    }`;
+  }
 
-    return req;
-})
+  return req;
+});
 
-export const fetchHolders = () => API.get('/holders');
-export const createHolder = (newHolder) => API.post('/holders', newHolder);
-export const updateHolder = (id, updatedHolder) => API.patch(`/holders/${id}`, updatedHolder);
-export const deleteHolder = (id) => API.delete(`/holders/${id}`)
+export const fetchHolders = () => API.get("/holders");
+export const createHolder = (newHolder) => API.post("/holders", newHolder);
+export const updateHolder = (id, updatedHolder) =>
+  API.patch(`/holders/${id}`, updatedHolder);
+export const deleteHolder = (id) => API.delete(`/holders/${id}`);
 
 //Auth
 
-export const signIn = (formData) => API.post('/user/signin', formData);
-export const signUp = (formData) => API.post('/user/signup', formData);
+export const signIn = (formData) => API.post("/user/signin", formData);
+export const signUp = (formData) => API.post("/user/signup", formData);
