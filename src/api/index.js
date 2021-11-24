0710e69cfe9d2f03c8884c918683bb6c20446b49
cafir <ecafirhan@gmail.com>
2021-11-25 03:01:57 +0700

    refactor

--- src/api/index.js
+++ src/api/index.js
@@ -1,11 +1,7 @@
 import axios from "axios";
 
-// const url = '/holders';
-
-
 const API = axios.create({
   baseURL: "https://keychain-backend.herokuapp.com/",
-  // baseURL: "http://localhost:5000",
 });
 
 API.interceptors.request.use((req) => {
